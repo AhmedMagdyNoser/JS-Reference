af7e7e4ae8c7b1c4ad83bba7ca73cd423a6fe8e1
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-06-30 07:08:46 +0300

    Update

--- Chapter 21 - Node Libraries/3 http Library/http.js
+++ Chapter 21 - Node Libraries/3 http Library/http.js
@@ -7,46 +7,42 @@ They provide functionality for handling incoming requests, sending responses, an
 */
 
 const fs = require('fs');
+const path = require('path');
 const http = require('http');
-
-let page;
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-
   // Handle different routes based on the requested URL
   switch (req.url) {
     case '/':
-      // If the requested URL is '/', set the page variable to the home.html file path
-      page = `${__dirname}/Pages/home.html`
+      serveHTMLFile(res, 200, 'index');
       break;
     case '/about':
-      // If the requested URL is '/about', set the page variable to the about.html file path
-      page = `${__dirname}/Pages/about.html`
+      serveHTMLFile(res, 200, 'about');
       break;
     case '/contact':
-      // If the requested URL is '/contact', set the page variable to the contact.html file path
-      page = `${__dirname}/Pages/contact.html`
+      serveHTMLFile(res, 200, 'contact');
       break;
     default:
-      // If the requested URL doesn't match any of the above cases, set the page variable to the error.html file path
-      page = `${__dirname}/Pages/error.html`
-      res.statusCode = 404; // Set the response status code to 404 (Not Found)
+      serveHTMLFile(res, 404, 'error');
       break;
   }
+});
 
-  // Read the contents of the specified HTML file
-  fs.readFile(page, 'utf-8', (err, data) => {
+function serveHTMLFile(res, status, fileName) {
+  const filePath = path.join(__dirname, 'pages', `${fileName}.html`);
+  fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-      console.log(err); // If an error occurs while reading the file, log the error to the console
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(err.toString());
     } else {
-      res.end(data); // Send the file contents as the response
+      res.writeHead(status, { 'Content-Type': 'text/html' });
+      res.end(data);
     }
   });
+}
 
-});
+const PORT = 5000;
 
-// Start the server and listen on the specified PORT and HOST
-server.listen(4000, 'localhost', () => {
-  console.log(`Server is running`);
-});
+// Start the server and listen on the specified PORT
+server.listen(PORT, 'localhost', () => console.log(`Server is running on port:${PORT}`));
